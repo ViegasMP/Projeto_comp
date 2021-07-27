ca7dbf7b86f2aae0abcca5dbcf88ae5c21053e63
Maria Paula Viegas <mpviegas23@gmail.com>
2021-07-27 17:07:57 +0100

    Add files via upload

---
+++ meta2/tree_functions.h
@@ -0,0 +1,19 @@
+#ifndef TREE_FUNCTIONS_H
+#define TREE_FUNCTIONS_H
+
+typedef struct no{
+	char *nome;
+	char *valor;
+	struct no* filho;
+	struct no* irmao;
+} No;
+
+No* cria_no(char* nome, char* valor);
+void add_irmao(No* n1, No* n2);
+void add_filho(No* n, No* filho);
+int check_irmao(No* n);
+void tratamentoIDRep(No* pai, No* filho);
+void print_tree(No* n, int nivel);
+void free_tree(No* n);
+
+#endif
